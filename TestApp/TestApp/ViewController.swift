87b167e282599ac0d2d20045d116c83b9d99289b
Tsuprik Olga <tt.olia@mail.ru>
2021-06-23 12:35:55 +0300

    Added JSON

---
+++ TestApp/TestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestApp
+//
+//  Created by Tsuprik Olga on 23.06.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
